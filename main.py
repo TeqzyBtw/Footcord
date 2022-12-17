b22de3669734e4be6a03ec66708c91898a381a3d
Teqzy <103995602+TeqzyBtw@users.noreply.github.com>
2022-12-17 14:16:34 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import nextcord
+import requests
+import asyncio
+import http.client
+
+conn = http.client.HTTPConnection("v3.football.api-sports.io")
+
+headers = {
+    'x-rapidapi-host': "v3.football.api-sports.io",
+    'x-rapidapikey': "API KEY HERE"
+}
+
+client = nextcord.Client()
+
+
+@client.event
+async def on_ready():
+    #await asyncio.sleep(2)
+    print(f"bot is online, ping: {client.latency}")
+    #while True:
+        #await asyncio.sleep(15)
+
+
+
+@client.slash_command(description="Sends The Current Countrys That Are Still Not Eliminated")
+async def countrys(interaction: nextcord.Interaction):
+    conn.request("GET", "/countries", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    stringcounter = data.decode("utf-8")
+    embed = nextcord.Embed(title="Current Countrys That Are Still In", description=f"{stringcounter}", color=nextcord.Color.green)
+
+
+client.run("TOKEN HERE")
